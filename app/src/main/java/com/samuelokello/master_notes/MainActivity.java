908e0076c0dd8e98357b7104f8e0ed09512c20a8
Samuel Okello <samuel.okello@studentambassadors.com>
2023-07-06 16:30:28 +0300

    java classes

--- app/src/main/java/com/samuelokello/master_notes/MainActivity.java
+++ app/src/main/java/com/samuelokello/master_notes/MainActivity.java
@@ -2,13 +2,24 @@ package com.samuelokello.master_notes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton addButton, deleteButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        addButton = findViewById(R.id.add_button);
+//        deleteButton = findViewById(delete_button);
+
+        addButton.setOnClickListener((v) -> startActivity(new Intent(MainActivity.this, NotedetailsActivity.class)));
+//        deleteButton.setOnClickListener((v) ->);
     }
 }
