d4ca00f665b21986ce8305f2a45436f046fa0d7f
Samuel Okello <samuel.okello@studentambassadors.com>
2023-07-21 19:52:46 +0300

    Convert to kotlin

--- app/src/main/java/com/samuelokello/master_notes/CreateAccount.kt
+++ app/src/main/java/com/samuelokello/master_notes/CreateAccount.kt
@@ -1,122 +1,123 @@
-package com.samuelokello.master_notes;
+package com.samuelokello.master_notes
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent
+import android.os.Bundle
+import android.util.Patterns
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.samuelokello.master_notes.Utility.showToast
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Patterns;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.firebase.auth.FirebaseAuth;
-
-public class CreateAccount extends AppCompatActivity {
-
-
-    EditText etUsername,etEmail, etPassword, etConfirmPassword;
-    Button btnSignUp;
-    ProgressBar progressBar;
-    TextView tvSignIn;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_account);
-
-        etUsername = findViewById(R.id.user_name_edit_text);
-        etEmail = findViewById(R.id.email_edit_text);
-        etPassword = findViewById(R.id.password_edit_text);
-        etConfirmPassword = findViewById(R.id.confirm_password_edit_text);
-        btnSignUp = findViewById(R.id.sign_up_btn);
-        progressBar = findViewById(R.id.progress_bar);
-        tvSignIn = findViewById(R.id.sign_In_textview_btn);
-
-        btnSignUp.setOnClickListener(v -> createAccount());
-        tvSignIn.setOnClickListener(v -> startActivity(new Intent(CreateAccount.this, LoginActivity.class)));
-
-    }
-
-
-        void createAccount() {
-            String username = etUsername.getText().toString();
-            String email = etEmail.getText().toString();
-            String password = etPassword.getText().toString();
-            String confirmPassword = etConfirmPassword.getText().toString();
-
-            boolean isValidated = validateData(username, email, password, confirmPassword);
-
-            if (!isValidated) {
-                return;
-            }
-
-            createAccountInFireBase(username,email,password);
-        }
-        void createAccountInFireBase(String username, String email, String password) {
-            changeInProgress(true);
-
-            FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
-            firebaseAuth.createUserWithEmailAndPassword(email, password)
-                    .addOnCompleteListener(task -> {
-                        changeInProgress(false);
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Utility.showToast(CreateAccount.this,"Successfully create account,Check email to verify");
-                            firebaseAuth.getCurrentUser().sendEmailVerification();
-                            firebaseAuth.signOut();
-                            finish();
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Utility.showToast(CreateAccount.this, task.getException().getLocalizedMessage());
-                        }
-                    });
-        }
-
-        void changeInProgress(boolean inProgress) {
-            if (inProgress) {
-                progressBar.setVisibility(View.VISIBLE);
-                btnSignUp.setOnClickListener(null);
-                btnSignUp.setVisibility(View.GONE);
-            } else {
-                progressBar.setVisibility(View.GONE);
-                btnSignUp.setOnClickListener(v -> createAccount());
-            }
-        }
-
-            boolean validateData(String username, String email, String password, String confirmPassword)
-            {
-                //validate username
-                if (username.isEmpty()) {
-                    etUsername.setError("Username is required");
-                    etUsername.requestFocus();
-                    return false;
-                }
-                //validate email
-                if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-                    etEmail.setError("Invalid Email");
-                    return false;
-                }
-
-                //validate password length
-                if (password.length() < 8) {
-                    etPassword.setError("Password should be at least 8 characters long");
-                    etPassword.requestFocus();
-                    return false;
-                }
-                //validate password match
-                if (!confirmPassword.equals(confirmPassword)) {
-                    etConfirmPassword.setError("Password do not match");
-                    etConfirmPassword.requestFocus();
-                    return false;
-                }
-
-                return true;
-            }
-
-
-        }
-
-
-
+class CreateAccount : AppCompatActivity() {
+	private var etUsername: EditText? = null
+	private var etEmail: EditText? = null
+	private var etPassword: EditText? = null
+	private var etConfirmPassword: EditText? = null
+	private var btnSignUp: Button? = null
+	private var progressBar: ProgressBar? = null
+	private var tvSignIn: TextView? = null
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_create_account)
+		etUsername = findViewById(R.id.user_name_edit_text)
+		etEmail = findViewById(R.id.email_edit_text)
+		etPassword = findViewById(R.id.password_edit_text)
+		etConfirmPassword = findViewById(R.id.confirm_password_edit_text)
+		btnSignUp = findViewById(R.id.sign_up_btn)
+		progressBar = findViewById(R.id.progress_bar)
+		tvSignIn = findViewById(R.id.sign_In_textview_btn)
+		with(btnSignUp){this?.setOnClickListener(View.OnClickListener { v: View? -> createAccount() })}
+		with(tvSignIn){this?.setOnClickListener(View.OnClickListener { v: View? ->
+			startActivity(
+				Intent(
+					this@CreateAccount,
+					LoginActivity::class.java
+				)
+			)
+		})}
+	}
+	
+	fun createAccount() {
+		val username = etUsername!!.text.toString()
+		val email = etEmail!!.text.toString()
+		val password = etPassword!!.text.toString()
+		val confirmPassword = etConfirmPassword!!.text.toString()
+		val isValidated = validateData(username, email, password, confirmPassword)
+		if (!isValidated) {
+			return
+		}
+		createAccountInFireBase(username, email, password)
+	}
+	
+	fun createAccountInFireBase(username: String?, email: String?, password: String?) {
+		changeInProgress(true)
+		val firebaseAuth = FirebaseAuth.getInstance()
+		firebaseAuth.createUserWithEmailAndPassword(email!!, password!!)
+			.addOnCompleteListener { task: Task<AuthResult?> ->
+				changeInProgress(false)
+				if (task.isSuccessful) {
+					// Sign in success, update UI with the signed-in user's information
+					showToast(
+						this@CreateAccount,
+						"Successfully create account,Check email to verify"
+					)
+					firebaseAuth.currentUser!!.sendEmailVerification()
+					firebaseAuth.signOut()
+					finish()
+				} else {
+					// If sign in fails, display a message to the user.
+					showToast(this@CreateAccount, task.exception!!.localizedMessage)
+				}
+			}
+	}
+	
+	fun changeInProgress(inProgress: Boolean) {
+		if (inProgress) {
+			progressBar!!.visibility = View.VISIBLE
+			btnSignUp!!.setOnClickListener(null)
+			btnSignUp!!.visibility = View.GONE
+		} else {
+			progressBar!!.visibility = View.GONE
+			btnSignUp!!.setOnClickListener { createAccount() }
+		}
+	}
+	
+	fun validateData(
+		username: String,
+		email: String?,
+		password: String,
+		confirmPassword: String
+	): Boolean {
+		//validate username
+		if (username.isEmpty()) {
+			etUsername!!.error = "Username is required"
+			etUsername!!.requestFocus()
+			return false
+		}
+		//validate email
+		if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+			etEmail!!.error = "Invalid Email"
+			return false
+		}
+		
+		//validate password length
+		if (password.length < 8) {
+			etPassword!!.error = "Password should be at least 8 characters long"
+			etPassword!!.requestFocus()
+			return false
+		}
+		//validate password match
+		if (confirmPassword != password) {
+			etConfirmPassword!!.error = "Password do not match"
+			etConfirmPassword!!.requestFocus()
+			return false
+		}
+		return true
+	}
+}
--- app/src/main/java/com/samuelokello/master_notes/LoginActivity.kt
+++ app/src/main/java/com/samuelokello/master_notes/LoginActivity.kt
@@ -1,112 +1,101 @@
-package com.samuelokello.master_notes;
+package com.samuelokello.master_notes
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent
+import android.os.Bundle
+import android.util.Patterns
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.samuelokello.master_notes.Utility.showToast
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Patterns;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-
-public class LoginActivity extends AppCompatActivity {
-
-    EditText etEmail, etPassword;
-    Button btnSignIn;
-    ProgressBar progressBar;
-    TextView tvSignUp, tvForgotPassword;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-        etEmail = findViewById(R.id.user_name_edit_text);
-        etPassword = findViewById(R.id.password_edit_text);
-        btnSignIn = findViewById(R.id.sign_in_btn);
-
-        progressBar = findViewById(R.id.progress_bar);
-        tvSignUp = findViewById(R.id.sign_up_textview_btn);
-//        tvForgotPasword = findViewById(R.id.forgot_password_textview_btn);
-
-        btnSignIn.setOnClickListener((v) -> loginUser());
-        tvSignUp.setOnClickListener((v) -> startActivity
-                (new Intent(LoginActivity.this, CreateAccount.class)));
-    }
-
-    void loginUser(){
-        String email = etEmail.getText().toString();
-        String password = etPassword.getText().toString();
-
-        boolean isValidated = validateData(email, password);
-
-        if (!isValidated) {
-            return;
-        }
-
-        loginAccountInFirebase(email,password);
-    }
-
-    void loginAccountInFirebase(String email, String password){
-        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
-        changeInProgress(true);
-        firebaseAuth.signInWithEmailAndPassword(email,password)
-                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        changeInProgress(false);
-                        if (task.isSuccessful()){
-                            if (firebaseAuth.getCurrentUser().isEmailVerified()){
-                                startActivity(new Intent(LoginActivity.this,MainActivity.class));
-                                finish();
-                            }else {
-                                Utility.showToast(LoginActivity.this,"Email not Verified, please verify");
-                            }
-                        }else {
-                            Utility.showToast(LoginActivity.this, task.getException().getLocalizedMessage());
-                        }
-                    }
-                });
-    }
-    void changeInProgress(boolean inProgress) {
-        if (inProgress) {
-            progressBar.setVisibility(View.VISIBLE);
-            btnSignIn.setOnClickListener(null);
-            btnSignIn.setVisibility(View.GONE);
-        } else {
-            progressBar.setVisibility(View.GONE);
-//            btnSignIn.setOnClickListener(v -> createAccount());
-        }
-    }
-
-    boolean validateData(String email, String password)
-    {
-
-        //validate email
-        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            etEmail.setError("Invalid Email");
-            return false;
-        }
-
-        //validate password length
-        if (password.length() < 8) {
-            etPassword.setError("Password should be at least 8 characters long");
-            etPassword.requestFocus();
-            return false;
-        }
-
-
-        return true;
-    }
-
-
+class LoginActivity : AppCompatActivity() {
+	var etEmail: EditText? = null
+	var etPassword: EditText? = null
+	var btnSignIn: Button? = null
+	var progressBar: ProgressBar? = null
+	var tvSignUp: TextView? = null
+	var tvForgotPassword: TextView? = null
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_login)
+		etEmail = findViewById(R.id.user_name_edit_text)
+		etPassword = findViewById(R.id.password_edit_text)
+		btnSignIn = findViewById(R.id.sign_in_btn)
+		progressBar = findViewById(R.id.progress_bar)
+		tvSignUp = findViewById(R.id.sign_up_textview_btn)
+		//        tvForgotPasword = findViewById(R.id.forgot_password_textview_btn);
+		with(btnSignIn){this?.setOnClickListener(View.OnClickListener { v: View? -> loginUser() })}
+		with( tvSignUp){this?.setOnClickListener(View.OnClickListener { v: View? ->
+			startActivity(
+				Intent(
+					this@LoginActivity,
+					CreateAccount::class.java
+				)
+			)
+		})}
+	}
+	
+	fun loginUser() {
+		val email = etEmail!!.text.toString()
+		val password = etPassword!!.text.toString()
+		val isValidated = validateData(email, password)
+		if (!isValidated) {
+			return
+		}
+		loginAccountInFirebase(email, password)
+	}
+	
+	fun loginAccountInFirebase(email: String?, password: String?) {
+		val firebaseAuth = FirebaseAuth.getInstance()
+		changeInProgress(true)
+		firebaseAuth.signInWithEmailAndPassword(email!!, password!!)
+			.addOnCompleteListener { task ->
+				changeInProgress(false)
+				if (task.isSuccessful) {
+					if (firebaseAuth.currentUser!!.isEmailVerified) {
+						startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+						finish()
+					} else {
+						showToast(this@LoginActivity, "Email not Verified, please verify")
+					}
+				} else {
+					showToast(
+						this@LoginActivity, task.exception!!
+							.localizedMessage
+					)
+				}
+			}
+	}
+	
+	fun changeInProgress(inProgress: Boolean) {
+		if (inProgress) {
+			progressBar!!.visibility = View.VISIBLE
+			btnSignIn!!.setOnClickListener(null)
+			btnSignIn!!.visibility = View.GONE
+		} else {
+			progressBar!!.visibility = View.GONE
+			//            btnSignIn.setOnClickListener(v -> createAccount());
+		}
+	}
+	
+	fun validateData(email: String?, password: String): Boolean {
+		
+		//validate email
+		if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+			etEmail!!.error = "Invalid Email"
+			return false
+		}
+		
+		//validate password length
+		if (password.length < 8) {
+			etPassword!!.error = "Password should be at least 8 characters long"
+			etPassword!!.requestFocus()
+			return false
+		}
+		return true
+	}
 }
