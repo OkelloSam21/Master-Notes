1127259bb002d9a20de191a56db69df001c4086c
Samuel Okello <samuel.okello@studentambassadors.com>
2023-07-21 19:26:07 +0300

    Convert Utility.java to kotlin

--- app/src/main/java/com/samuelokello/master_notes/Utility.kt
+++ app/src/main/java/com/samuelokello/master_notes/Utility.kt
@@ -1,27 +1,28 @@
-package com.samuelokello.master_notes;
+package com.samuelokello.master_notes
 
-import android.content.Context;
-import android.widget.Toast;
+import android.content.Context
+import android.widget.Toast
+import com.google.firebase.Timestamp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.FirebaseFirestore
+import java.text.SimpleDateFormat
 
-import com.google.firebase.Timestamp;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.FirebaseFirestore;
-
-import java.text.SimpleDateFormat;
-
-public class Utility {
-    static void showToast(Context context, String message){
-        Toast.makeText(context,message,Toast.LENGTH_SHORT).show();
-    }
-    static CollectionReference getCollectionReferenceForNotes(){
-        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
-        return FirebaseFirestore.getInstance().collection("notes")
-                .document(currentUser.getUid()).collection("my_notes");
-    }
-
-    static String timestampToString(Timestamp timestamp){
-        return new SimpleDateFormat("MM/dd/yyyy").format(timestamp.toDate());
-    }
+object Utility {
+	@JvmStatic
+    fun showToast(context: Context?, message: String?) {
+		Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+	}
+	
+	@JvmStatic
+    val collectionReferenceForNotes: CollectionReference
+		get() {
+			val currentUser = FirebaseAuth.getInstance().currentUser
+			return FirebaseFirestore.getInstance().collection("notes")
+				.document(currentUser!!.uid).collection("my_notes")
+		}
+	
+	fun timestampToString(timestamp: Timestamp): String {
+		return SimpleDateFormat("MM/dd/yyyy").format(timestamp.toDate())
+	}
 }
