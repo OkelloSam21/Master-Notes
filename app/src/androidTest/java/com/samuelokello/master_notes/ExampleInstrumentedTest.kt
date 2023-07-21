d13503890801a7c7f070839a9c413fd6f248b9e8
Samuel Okello <samuel.okello@studentambassadors.com>
2023-07-21 20:00:27 +0300

    Convert to kotlin

--- app/src/androidTest/java/com/samuelokello/master_notes/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/samuelokello/master_notes/ExampleInstrumentedTest.kt
@@ -1,26 +1,22 @@
-package com.samuelokello.master_notes;
+package com.samuelokello.master_notes
 
-import android.content.Context;
-
-import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * @see [Testing documentation](http://d.android.com/tools/testing)
  */
-@RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.samuelokello.master_notes", appContext.getPackageName());
-    }
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+	@Test
+	fun useAppContext() {
+		// Context of the app under test.
+		val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+		Assert.assertEquals("com.samuelokello.master_notes", appContext.packageName)
+	}
 }
